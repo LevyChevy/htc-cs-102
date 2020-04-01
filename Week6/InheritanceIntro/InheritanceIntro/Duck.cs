02a71c6d989e8b1b8ab9133b185b5dd85f8415e1
Levi Mcfetridge <mcfetridgel12@bereaschools.org>
2020-04-01 12:22:04 -0400

    Added duck and animal classes

---
+++ Week6/InheritanceIntro/InheritanceIntro/Duck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InheritanceIntro
+{
+    class Duck : Animal
+    {
+        //method
+        public void Quack()
+        {
+            MessageBox.Show("Quack");
+        }
+        //constructers
+
+        public Duck()
+        {
+
+        }
+
+        public Duck(int height, string name)
+        {
+            Height = height;
+            Name = name;
+        }
+
+    }
+}
