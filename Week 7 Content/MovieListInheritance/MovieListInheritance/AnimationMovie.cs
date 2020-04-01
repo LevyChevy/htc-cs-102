2ff893f6d14dddb5df8f88bc0c64ffd3fe3d44fc
Levi Mcfetridge <mcfetridgel12@bereaschools.org>
2020-04-01 18:34:09 -0400

    Animation movie stuff

---
+++ Week 7 Content/MovieListInheritance/MovieListInheritance/AnimationMovie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieListInheritance
+{
+     public class AnimationMovie : Movie
+    {
+        public string AnimationStudio;
+        public string AnimationType;
+
+        public AnimationMovie()
+        {
+
+        }
+        public AnimationMovie(string animationstudio, string animationtype, string title, int releaseyear)
+        {
+            AnimationStudio = animationstudio;
+            AnimationType = animationtype;
+            Title = title;
+            ReleaseYear = releaseyear;
+        }
+    }
+}
